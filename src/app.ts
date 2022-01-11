df1300e8d70fe3f26efb0aa38e3219c530f16ebf
moussa <mmoussasow@gmail.com>
2022-01-11 17:58:46 +0100

    node-express squelette

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
-import express from 'express';
-import { toto } from './ajv';
+import express, { Request, Response } from "express";
 
-toto();
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("The sedulous hyena ate the antelope!");
 });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
